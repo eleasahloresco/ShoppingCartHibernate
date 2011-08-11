df78e3d2317d5c3b8064f4f2020da48143029f7f
Eleasah <eleasah.loresco@orangeandbronze.com>
2011-08-11 17:18:45 +0800

    Created OrderDetailDao, OrderDetailDaoTest

---
+++ src/com/onb/shoppingcart/dao/OrderDetailDao.java
@@ -0,0 +1,7 @@
+package com.onb.shoppingcart.dao;
+
+import com.onb.shoppingcart.domain.OrderDetail;
+
+public interface OrderDetailDao extends DAO<OrderDetail, Long>{
+
+}
---
+++ src/com/onb/shoppingcart/dao/impl/OrderDetailDaoImpl.java
@@ -0,0 +1,15 @@
+package com.onb.shoppingcart.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.onb.shoppingcart.dao.OrderDetailDao;
+import com.onb.shoppingcart.domain.OrderDetail;
+
+@Repository("orderDetailDao")
+public class OrderDetailDaoImpl extends AbstractDao<OrderDetail, Long> implements OrderDetailDao {
+
+	protected OrderDetailDaoImpl() {
+		super(OrderDetail.class);
+	}
+
+}
